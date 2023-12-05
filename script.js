a53045f1dcc5a3dc6dc7254125ff4f2a45b0d2d7
Yojxmbo <125270348+Yojxmbo@users.noreply.github.com>
2023-12-05 01:49:41 -0600

    Second commit. Pages complete minus images

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- 
+document.addEventListener('DOMContentLoaded', function () {
+    var email = document.getElementById('email');
+    var confirmEmail = document.getElementById('confirmEmail');
+
+    email.addEventListener('input', function () {
+        // Check if the email and confirmEmail fields match
+        if (email.value !== confirmEmail.value) {
+            confirmEmail.setCustomValidity('Email addresses must match.');
+        } else {
+            confirmEmail.setCustomValidity('');
+        }
+    });
+
+    confirmEmail.addEventListener('input', function () {
+        // Check if the email and confirmEmail fields match
+        if (email.value !== confirmEmail.value) {
+            confirmEmail.setCustomValidity('Email addresses must match.');
+        } else {
+            confirmEmail.setCustomValidity('');
+        }
+    });
+});
+
