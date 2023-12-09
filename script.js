d0e74ec73059cde54870c4e9284b1f92fba0e8c1
Yojxmbo <125270348+Yojxmbo@users.noreply.github.com>
2023-12-09 06:14:36 -0600

    second revision

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ document.addEventListener('DOMContentLoaded', function () {
         // Check if the email and confirmEmail fields match
         if (email.value !== confirmEmail.value) {
             confirmEmail.setCustomValidity('Email addresses must match.');
+
+            // Change border color to indicate non-matching emails
+            confirmEmail.style.borderColor = 'red';
+
+            // Optionally, display an alert to the user
+            alert('Email addresses do not match. Please enter matching email addresses.');
         } else {
             confirmEmail.setCustomValidity('');
+
+            // Set border color to green to indicate matching emails
+            confirmEmail.style.borderColor = 'green';
+
+            // Display a verification message
+            alert('Email addresses match. Verification successful.');
+
+            // You can also display a message on the page if needed
+            // Example: document.getElementById('verificationMessage').innerText = 'Email addresses match. Verification successful.';
         }
     }
 
-    email.addEventListener('input', checkEmailMatch);
-    confirmEmail.addEventListener('input', checkEmailMatch);
+    email.addEventListener('change', checkEmailMatch);
+    confirmEmail.addEventListener('change', checkEmailMatch);
 });
